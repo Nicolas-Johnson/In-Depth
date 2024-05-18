fd0392c62508c743d830c4714b68cd2919c3409a
Nicolas Johnson <nicolasssousa2011@gmail.com>
2024-05-18 20:03:00 -0300

    Starter

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './sass/styles.scss'
+import { Horse, Heart, Cube } from "@phosphor-icons/react";
+
+function App() {
+
+  return (
+    <h1>In Depth port Folio<br/><span><Horse /></span><br/><span><Heart /></span><br/><span><Cube /></span></h1>
+  )
+}
+
+export default App
